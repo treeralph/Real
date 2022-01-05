6c558ec00f4dcf06a3571354ea712e6d06e5d74f
JUN <treeralph@gmail.com>
2022-01-05 17:37:46 +0900

    LoadingActivity 24 Line에 주석으로 박강혁 바보멍청이라고 써놓음.

--- app/src/main/java/com/example/real/LoadingActivity.java
+++ app/src/main/java/com/example/real/LoadingActivity.java
@@ -21,7 +21,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class LoadingActivity extends Activity {
 
-    // 박강혁 바보
+    // 박강혁 바보 멍청이
     public static Context LoadingContext;
     LoadingHandler handler;
     LottieAnimationView lottie;
