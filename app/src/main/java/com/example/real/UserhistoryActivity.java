8fc1ad7dc305c2631b032beb6c2ffa36c1cf8272
TheCodeMasterPark <parkkanghyeok95@gmail.com>
2022-01-06 16:59:00 +0900

    Create Intent to UserhistoryActivity At ContentsActivity/popup

---
+++ app/src/main/java/com/example/real/UserhistoryActivity.java
@@ -0,0 +1,14 @@
+package com.example.real;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserhistoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_userhistory);
+    }
+}
