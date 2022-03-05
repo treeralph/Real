18d9b7565414b062f3b896439a97db2c74cdc171
JUN <treeralph@gmail.com>
2022-03-05 15:03:41 +0900

    Mutex Transaction for bid

--- app/src/main/java/com/example/real/tool/MyFirebaseMessagingService.java
+++ app/src/main/java/com/example/real/tool/MyFirebaseMessagingService.java
@@ -3,11 +3,14 @@ package com.example.real.tool;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.core.app.NotificationManagerCompat;
 
+import com.example.real.AuctionBidActivity;
 import com.example.real.Callback;
 import com.example.real.R;
 import com.example.real.data.UserProfile;
@@ -27,6 +30,7 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
     @Override
     public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
+
         String title = remoteMessage.getNotification().getTitle();
         String body = remoteMessage.getNotification().getBody();
         final String CHANNEL_ID = "HEADS_UP_NOTIFICATION";
@@ -45,58 +49,74 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         super.onMessageReceived(remoteMessage);
 
         Map<String, String> payload = remoteMessage.getData();
-        String[] messagePath = payload.get("path").split("/");
-        String chatRoomPath = "/" + messagePath[1] + "/" + messagePath[2] + "/" + messagePath[3];
-        String fromUid = payload.get("fromUid");
-        String toUid = payload.get("toUid");
-        String message = payload.get("message");
-        String time = payload.get("time");
-        String fromToken = payload.get("fromToken");
-        String toToken = payload.get("toToken");
-        String imageUri = payload.get("imageUri");
+        String flag = payload.get("wFCM");
 
-        firestoreManagerForUserProfile.read("UserProfile", fromUid, new Callback() {
-            @Override
-            public void OnCallback(Object object) {
-                UserProfile fromUser = (UserProfile)object;
-                ArrayList<String> ChattingRoomID = fromUser.getChattingRoomID();
-                if(ChattingRoomID.contains(chatRoomPath) == false) {
-                    ChattingRoomID.add(chatRoomPath);
-                    firestoreManagerForUserProfile.update("UserProfile", fromUid, "ChattingRoomID", ChattingRoomID, new Callback() {
-                        @Override
-                        public void OnCallback(Object object) {
-                            int LogMessage = (int) object;
-                            if (LogMessage == 0) {
-                                Log.d(TAG, "ISSUCCESSFUL");
-                            } else {
-                                Log.d(TAG, "ISFAILURE");
-                            }
-                        }
-                    });
-                }
-            }
-        });
+        if(flag.equals("ExistMessage")) {
 
-        firestoreManagerForUserProfile.read("UserProfile", toUid, new Callback() {
-            @Override
-            public void OnCallback(Object object) {
-                UserProfile fromUser = (UserProfile)object;
-                ArrayList<String> ChattingRoomID = fromUser.getChattingRoomID();
-                if(ChattingRoomID.contains(chatRoomPath) == false) {
-                    ChattingRoomID.add(chatRoomPath);
-                    firestoreManagerForUserProfile.update("UserProfile", toUid, "ChattingRoomID", ChattingRoomID, new Callback() {
-                        @Override
-                        public void OnCallback(Object object) {
-                            int LogMessage = (int) object;
-                            if (LogMessage == 0) {
-                                Log.d(TAG, "ISSUCCESSFUL");
-                            } else {
-                                Log.d(TAG, "ISFAILURE");
+            //Map<String, String> payload = remoteMessage.getData();
+            String[] messagePath = payload.get("path").split("/");
+            String chatRoomPath = "/" + messagePath[1] + "/" + messagePath[2] + "/" + messagePath[3];
+            String fromUid = payload.get("fromUid");
+            String toUid = payload.get("toUid");
+            String message = payload.get("message");
+            String time = payload.get("time");
+            String fromToken = payload.get("fromToken");
+            String toToken = payload.get("toToken");
+            String imageUri = payload.get("imageUri");
+
+            firestoreManagerForUserProfile.read("UserProfile", fromUid, new Callback() {
+                @Override
+                public void OnCallback(Object object) {
+                    UserProfile fromUser = (UserProfile) object;
+                    ArrayList<String> ChattingRoomID = fromUser.getChattingRoomID();
+                    if (ChattingRoomID.contains(chatRoomPath) == false) {
+                        ChattingRoomID.add(chatRoomPath);
+                        firestoreManagerForUserProfile.update("UserProfile", fromUid, "ChattingRoomID", ChattingRoomID, new Callback() {
+                            @Override
+                            public void OnCallback(Object object) {
+                                int LogMessage = (int) object;
+                                if (LogMessage == 0) {
+                                    Log.d(TAG, "ISSUCCESSFUL");
+                                } else {
+                                    Log.d(TAG, "ISFAILURE");
+                                }
                             }
-                        }
-                    });
+                        });
+                    }
                 }
-            }
-        });
+            });
+
+            firestoreManagerForUserProfile.read("UserProfile", toUid, new Callback() {
+                @Override
+                public void OnCallback(Object object) {
+                    UserProfile fromUser = (UserProfile) object;
+                    ArrayList<String> ChattingRoomID = fromUser.getChattingRoomID();
+                    if (ChattingRoomID.contains(chatRoomPath) == false) {
+                        ChattingRoomID.add(chatRoomPath);
+                        firestoreManagerForUserProfile.update("UserProfile", toUid, "ChattingRoomID", ChattingRoomID, new Callback() {
+                            @Override
+                            public void OnCallback(Object object) {
+                                int LogMessage = (int) object;
+                                if (LogMessage == 0) {
+                                    Log.d(TAG, "ISSUCCESSFUL");
+                                } else {
+                                    Log.d(TAG, "ISFAILURE");
+                                }
+                            }
+                        });
+                    }
+                }
+            });
+        } else if(flag.equals("ExistUpperBidUser")){
+            sendNotificationForExistUpperBidUser(payload.get("contentId"));
+        } else{
+
+        }
+    }
+
+    private void sendNotificationForExistUpperBidUser(String msg){
+        Intent intent = new Intent(this, AuctionBidActivity.class);
+        intent.putExtra("ContentId", msg);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 1000, intent, PendingIntent.FLAG_ONE_SHOT);
     }
 }
